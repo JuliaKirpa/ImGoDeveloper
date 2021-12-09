5dafa8b4db5f852e4ca6a71432122e974c70bb8c
Julia Kirpa <lia_shongmao@mail.ru>
2021-12-09 15:35:15 +0300

    add handlers, update server

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"ImGoDeveloper"
+	"ImGoDeveloper/pkg/handler"
 	"log"
 )
 
-func main(){
+func main() {
+	handlers := new(handler.Handler)
 	srv := new(ImGoDeveloper.Server)
-
-	if err := srv.Run("8080"); err != nil {
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error while running http server: %s", err.Error())
 	}
+
 }
