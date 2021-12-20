08cac965995272fb7b768b99c68fa472beedf79a
Gipernova <lia_shongmao@mail.ru>
2021-12-20 19:07:10 +0300

    correct mistake

--- pkg/repository/auth_postgres.go
+++ pkg/repository/auth_postgres.go
@@ -16,7 +16,7 @@ func NewAuthPostgres(db *sqlx.DB) *AuthPostgres {
 
 func (r *AuthPostgres) CreateUser(user ImGoDeveloper.User) (int, error) {
 	var id int
-	qwery := fmt.Sprintf("INSERT INTO %s (name, usermame, password_hash) values ($1, $2, $3) RETURNING id", usersTable)
+	qwery := fmt.Sprintf("INSERT INTO %s (name, username, password_hash) values ($1, $2, $3) RETURNING id", usersTable)
 	row := r.db.QueryRow(qwery, user.Name, user.Username, user.Password)
 
 	if err := row.Scan(&id); err != nil {
